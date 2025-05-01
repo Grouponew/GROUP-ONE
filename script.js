15358b374af8bdace7ea88d7ae81052774d40024
Grouponew <njokufortune27@gmail.com>
2025-05-01 10:11:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-<script>
-            function validateform(event){
-                alert("Thank You!");
-                event.preventdefault();
-            }
-        </script>
-        
+function validateform(event) {
+  alert("Thank You!");
+  event.preventefault();
+}
